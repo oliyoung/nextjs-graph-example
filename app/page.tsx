67bb6012f14a1f6818609d2ed1d8bed8c00e7a80
Oli Young <oliyoung@gmail.com>
2024-10-17 21:22:39 +1030

    Updated query, rebuild Character block

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { useQuery } from '@apollo/client';
-import Character from '@/components/Character';
+import CharacterBlock from '@/components/Character';
 import Pagination from '@/components/Pagination';
 import { useSearchParams } from 'next/navigation'
-import { Grid, GridItem } from '@chakra-ui/react'
+import { BreadcrumbCurrentLink, BreadcrumbLink, BreadcrumbRoot, Grid, GridItem } from '@chakra-ui/react'
 
 import { gql } from '@/__generated__/gql';
+import { Character } from '@/types';
 
 const getCharactersQuery = gql(`
     query getCharacters($page: Int!) {
@@ -20,7 +21,6 @@ const getCharactersQuery = gql(`
         name
         status
         species
-        type
         gender
         image
         location {
@@ -40,10 +40,14 @@ const Page = () => {
   }
 
   return <main>
-    <Grid templateColumns={['1', 'repeat(3, 1fr)', 'repeat(4, 1fr)', 'repeat(5, 1fr)']} gap={[2, 6]}>
+    <BreadcrumbRoot marginBottom={4}>
+      <BreadcrumbLink href="/">Home</BreadcrumbLink>
+      <BreadcrumbCurrentLink>Characters</BreadcrumbCurrentLink>
+    </BreadcrumbRoot>
+    <Grid templateColumns={['1', '1', 'repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)']} gap={[2, 6]}>
       {data?.characters?.results?.map((character) =>
         character && <GridItem>
-          <Character character={character} />
+          <CharacterBlock character={character as Character} />
         </GridItem>)}
     </Grid>
 
--- components/Character.tsx
+++ components/Character.tsx
@@ -1,12 +1,17 @@
-import { Character } from "@/types"
+import { Character, Maybe } from "@/types"
 import { Card } from "@chakra-ui/react"
+import { DataListItem, DataListRoot } from "./ui/data-list"
 
-const CharacterBlock = ({ character }: { character: Omit<Character, 'location' | 'episode'> }) => {
+const CharacterBlock = ({ character }: { character: Character }) => {
     return <Card.Root w={'100%'} h='100%'>
         <Card.Header>{character.name}</Card.Header>
         <Card.Body>
-            {character.gender}
-            {character.status}
+            <DataListRoot orientation="horizontal">
+                <DataListItem key='location' label='Location' value={character?.location?.name} />
+                <DataListItem key='gender' label='Gender' value={character.gender} />
+                <DataListItem key='status' label='Status' value={character.status} />
+                <DataListItem key='species' label='Species' value={character.species} />
+            </DataListRoot>
         </Card.Body>
     </Card.Root>
 }

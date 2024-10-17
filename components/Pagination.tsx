38aaaf5cc6cd3022d60453a466aa896c9660116f
Oli Young <oliyoung@gmail.com>
2024-10-17 22:11:42 +1030

    Pagination was attempting to access window object too early throwing a rendering error, useEffect to ensure we're in a browser

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -3,18 +3,21 @@
 import {
     PaginationItems,
     PaginationNextTrigger,
-    PaginationPageText,
     PaginationPrevTrigger,
     PaginationRoot,
 } from "@/components/ui/pagination"
 import { HStack, VStack } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 const Pagination = ({ currentPage, pageCount }: { currentPage: number, pageCount: number }) => {
-    if (!window?.location?.href) {
+    const [url, setUrl] = useState<URL>();
+    useEffect(() => {
+        setUrl(new URL(window?.location?.href));
+    }, [])
+
+    if (!url) {
         return <></>
     }
-
-    const url = new URL(window?.location?.href);
 
     return <PaginationRoot
         pageSize={1}
